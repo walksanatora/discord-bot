70d0b755dd9078890d3e7c54b59545634c7ba1f3
walksanatora <walkerffo22@hotmail.com>
2021-05-03 12:51:12 -0700

    the start

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 const discord = require('discord.js')
 require('dotenv').config()
 const client = new discord.Client();
+
+var PREFIX="?"
+
 console.log(typeof(process.env.DISCORD_TOKEN))
 console.log(process.env.DISCORD_TOKEN)
 client.once('ready', () => {
@@ -9,6 +12,17 @@ client.once('ready', () => {
 
 client.on('message', message => {
 	console.log(message.content);
+	cmds=message.content.split(' ')
+	switch(cmds[0]){
+		case `${PREFIX}test`:
+			message.channel.send("message recieved")
+		break;
+		case `${PREFIX}owo`:
+			message.channel.send("dont make me kill you weeb")
+		break;
+		default:
+			console.log("none of my buisness")
+	}
 });
 
 client.login(process.env.DISCORD_TOKEN);

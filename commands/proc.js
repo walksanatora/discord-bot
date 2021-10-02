3f9a5349d2001a528cbaa36221801b1c4a561e12
walksanatora <walkerffo22@hotmail.com>
2021-10-01 17:28:06 -0700

    require os

--- commands/proc.js
+++ commands/proc.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const discord = require('discord.js')
+const os = require('os')
 
 //utility function for converting bytes to a human-readable format
 function formatBytes(a,b=2,k=1024){with(Math){let d=floor(log(a)/log(k));return 0==a?"0 Bytes":parseFloat((a/pow(k,d)).toFixed(max(0,b)))+" "+["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"][d]}}
